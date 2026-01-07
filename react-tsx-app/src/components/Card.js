d5873840661dcb4643685af684d10bdc21b2626b
codespell <developer@codespell.ai>
2026-01-07 13:09:11 +0000

    feat: implement changes

---
+++ react-tsx-app/src/components/Card.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Card.module.css';
+
+const Card = ({ 
+  title, 
+  description, 
+  image, 
+  onClick, 
+  className 
+}) => {
+  return (
+    <article 
+      className={`${styles.card} ${className || ''}`} 
+      onClick={onClick}
+    >
+      {image && (
+        <div className={styles.cardImage}>
+          <img src={image} alt={title} />
+        </div>
+      )}
+      <div className={styles.cardContent}>
+        <h3 className={styles.cardTitle}>{title}</h3>
+        <p className={styles.cardDescription}>{description}</p>
+      </div>
+    </article>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  onClick: PropTypes.func,
+  className: PropTypes.string
+};
+
+Card.defaultProps = {
+  image: null,
+  onClick: () => {},
+  className: ''
+};
+
+export default Card;
