4c04ec1c2b70cdba127b1cd9361f431ea3d92cf4
codespell <developer@codespell.ai>
2026-01-08 17:50:53 +0530

    feat: implement changes

--- react-tsx-app/src/App.tsx
+++ react-tsx-app/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://www.metmuseum.org/art/collection/search/56353" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
